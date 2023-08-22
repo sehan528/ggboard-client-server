8989d4ebfde34a8670e6ff17296d2bb9792cdff1
jang se chang <sehan528@gmail.com>
2023-08-22 22:08:09 +0900

    vercel update v4

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,25 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+
+	build: {
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, 'routes/app.html'),
+				// nested: resolve(__dirname, 'nested/index.html'),
+			},
+		},
+	},
 });
+
+// export default defineConfig({
+// 	build: {
+// 		rollupOptions: {
+// 			input: {
+// 				main: resolve(__dirname, 'index.html'),
+// 				nested: resolve(__dirname, 'nested/index.html'),
+// 			},
+// 		},
+// 	},
+// })
